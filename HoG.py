34a32b805d645c08b0b78752e9dfb211195e56ee
Daniel Rice <dwrice.ncsu.edu>
2017-03-02 18:40:17 -0500

    Adding my working on HoG and OpticalFlow. sphere and tennis pictures are for optical flow. Will convert python files to jupyter in near future. For HoG and OpticalFlow, I just tried to mimic what the library functions do for both of them.

---
+++ HoG.py
@@ -0,0 +1,326 @@
+import cv2
+from scipy import misc
+import matplotlib.pyplot as plt
+import os
+import numpy as np
+import math
+from sklearn import datasets, svm, metrics, tree, ensemble, linear_model
+import glob
+
+
+'''
+x = np.uint8(253)
+y = np.uint8(255)
+grad = np.int16(x) - np.int16(y)
+gradMag = np.sqrt(grad **2)
+gradMag = np.uint8(min(gradMag, 255))
+print gradMag
+exit(0)
+'''
+CELL_WIDTH = 7
+CELL_HEIGHT = 7
+
+rootdir_training = 'mnist_png/training'
+rootdir_testing = 'mnist_png/testing'
+
+# need to deal with underflow (and probably overflow as well)
+
+'''
+Make sure we don't access outside of array
+'''
+def boundIndex(index, maxIndex):
+    if index < 0:
+        return 0
+    if index >= maxIndex:
+        return maxIndex - 1
+    return index
+
+
+'''
+Still need to work on the overflow errors
+Now I'm passing all of them in
+'''
+def apply_kernel(pixels):
+    rows = len(pixels)
+    columns = len(pixels[0])
+    gradient = np.array([[0 for x in range(columns)]for y in range(rows)], dtype=np.uint16)
+    angles = np.array([[0 for x in range(columns)]for y in range(rows)], dtype=np.uint8)
+    for i in range(0, rows):
+        for j in range(0, columns):
+            max = np.uint8(0)
+            angle = 0
+            # still need to figure out if want to use separate array for angles
+            for k in range(0,3):
+                # i realize I only need to check 0 or max, not both
+                rIndex = boundIndex(j+1, columns)
+                lIndex = boundIndex(j-1, columns)
+                tIndex = boundIndex(i-1, rows)
+                bIndex = boundIndex(i+1, rows)
+                leftValue = np.int16(pixels[i][lIndex][k])
+                rightValue = np.int16(pixels[i][rIndex][k])
+                topNeighbor = np.int16(pixels[tIndex][j][k])
+                botNeighbor = np.int16(pixels[bIndex][j][k])
+                gradientX = rightValue - leftValue
+                gradientY = botNeighbor - topNeighbor
+                gradMag = np.sqrt(gradientX ** 2 + gradientY ** 2)
+                #gradMag = np.uint8(min(gradMag, 255))
+                if gradMag > max:
+                    max=gradMag
+                    angle = np.arctan2(gradientY, gradientX)
+                    angle = angle * 180 / np.pi
+                    angle = np.uint8(abs(angle))
+            gradient[i][j] = max
+            angles[i][j] = angle
+
+    return gradient, angles
+
+def calc_hogs(gradients, angles):
+    '''
+    0 -> angle between 0 and 20
+    1 -> angle between 20 and 40
+    2 -> angle between 40 and 60
+    3 -> angle between 60 and 80
+    4 -> angle between 80 and 100
+    5 -> angle between 100 and 120
+    6 -> angle between 120 and 140
+    7 -> angle between 140 and 160
+    8 -> angle between 160 and 180
+    '''
+    hog = np.array([0 for x in range(9)])
+    rows = len(gradients)
+    columns = len(gradients[0])
+    for i in range(0, rows):
+        for j in range(0, columns):
+            # terrible code to follow -> break out into own function later
+            if angles[i][j] < 20:
+                hog[0] += gradients[i][j] * (1 - angles[i][j] / 20)
+                hog[1] += gradients[i][j] * (angles[i][j] / 20)
+            elif angles[i][j] < 40:
+                hog[1] += gradients[i][j] * (1-(angles[i][j]-20) / 20)
+                hog[2] += gradients[i][j] * ((angles[i][j] - 20)/20)
+            elif angles[i][j] < 60:
+                hog[2] += gradients[i][j] * (1-(angles[i][j] - 40)/20)
+                hog[3] += gradients[i][j] * ((angles[i][j] - 40)/20)
+            elif angles[i][j] < 80:
+                hog[3] += gradients[i][j] * (1-(angles[i][j] - 60)/20)
+                hog[4] += gradients[i][j] * ((angles[i][j] - 60)/20)
+            elif angles[i][j] < 100:
+                hog[4] += gradients[i][j] * (1-(angles[i][j] - 80)/20)
+                hog[5] += gradients[i][j] * ((angles[i][j]-80)/20)
+            elif angles[i][j] < 120:
+                hog[5] += gradients[i][j] * (1-(angles[i][j] - 100)/20)
+                hog[6] += gradients[i][j] * ((angles[i][j] - 100)/20)
+            elif angles[i][j] < 140:
+                hog[6] += gradients[i][j] * (1-(angles[i][j] - 120)/20)
+                hog[7] += gradients[i][j] * ((angles[i][j] - 120)/20)
+            elif angles[i][j] < 160:
+                hog[7] += gradients[i][j] * (1-(angles[i][j] - 140)/20)
+                hog[8] += gradients[i][j] * ((angles[i][j] - 140)/20)
+            elif angles[i][j] <= 180:
+                hog[8] += gradients[i][j] * (1-(angles[i][j] - 160)/20)
+                hog[0] += gradients[i][j] * ((angles[i][j] - 160)/20)
+
+    return hog
+
+def bin_gradients(gradients, angles):
+    rows = len(gradients)
+    columns = len(gradients[0])
+    combined_hog = np.zeros((rows/CELL_HEIGHT, columns/CELL_WIDTH, 9))
+    row_num = 0
+    for i in range(0, rows, CELL_HEIGHT):
+        col_num = 0
+        for j in range(0, columns, CELL_WIDTH):
+            cell_gradients = gradients[i:i+CELL_HEIGHT, j:j+CELL_WIDTH]
+            cell_angles = angles[i:i+CELL_HEIGHT, j:j+CELL_WIDTH]
+            hog = calc_hogs(cell_gradients, cell_angles)
+            combined_hog[row_num][col_num] = hog
+            col_num += 1
+        row_num += 1
+    return combined_hog
+
+
+def normalize_hog(hogs):
+    # need to combine the 4 groups of 9 into 1 group of 36 and normalize it
+    # how many arrays of size 36 will we need?
+    rows = len(hogs)
+    columns = len(hogs[0])
+    normalized_hog = np.zeros((rows - 1, columns - 1, 36))
+    # need to make it 1 less in both directions
+    for i in range(0, rows - 1):
+        for j in range(0, columns - 1):
+            # normalize it
+            cell1 = hogs[i][j]
+            cell2 = hogs[i][j+1]
+            cell3 = hogs[i+1][j]
+            cell4 = hogs[i+1][j+1]
+            magnitude = 0
+            # numpy has to have a better way of doing this, but for now..
+            for k in range(0, 9):
+                magnitude += (cell1[k] * cell1[k] + cell2[k] * cell2[k] + cell3[k] * cell3[k] + cell4[k] * cell4[k])
+            magnitude = math.sqrt(magnitude)
+            cells = np.concatenate([cell1, cell2])
+            cells = np.concatenate([cells, cell3])
+            cells = np.concatenate([cells, cell4])
+            for k in range(0, 36):
+                if magnitude > 0:
+                    normalized_hog[i][j][k] = cells[k] / magnitude
+                else:
+                    normalized_hog[i][j][k] = cells[k]
+    return normalized_hog
+
+'''
+Will concatenate a 3d array into 1d array
+'''
+def concatenate(array):
+    rows = len(array)
+    columns = len(array[0])
+    concatenated_array = np.array((rows*columns*len(array[0][0])))
+    concat_before = False
+    for i in range(0, rows):
+        for j in range(0, columns):
+            if (not concat_before):
+                concatenated_array = array[i][j]
+                concat_before = True
+            else:
+                concatenated_array = np.concatenate([concatenated_array, array[i][j]])
+    return concatenated_array
+
+
+test_image_path = os.path.abspath("mnist_png/testing/0/3.png")
+
+rgb = cv2.imread(test_image_path)
+'''
+rgb = cv2.resize(rgb, (0,0), fx=1, fy=2)
+plt.imshow(rgb)
+plt.show()
+exit(0)
+'''
+r = rgb[:,:,0]
+g = rgb[:,:,1]
+b = rgb[:,:,2]
+
+# calculate gradients for each of r, g, b
+gradient, angles = apply_kernel(rgb)
+
+
+
+hog = bin_gradients(gradient, angles)
+
+hog = normalize_hog(hog)
+
+
+hog = concatenate(hog)
+
+
+winSize = (28, 28)
+blockSize = (14,14)
+blockStride = (7,7)
+cellSize = (7,7)
+nbins = 9
+derivAperture = 1
+winSigma = 4.
+histogramNormType = 0
+L2HysThreshold = 2.0000000000000001e-01
+gammaCorrection = True
+
+
+
+hog2 = cv2.HOGDescriptor(winSize,blockSize,blockStride,cellSize,nbins)#,derivAperture,winSigma,
+                        #histogramNormType,L2HysThreshold,gammaCorrection)
+
+
+'''
+
+plt.imshow(rgb)
+plt.show()
+
+plt.imshow(gradient)
+plt.show()
+'''
+
+'''
+img_paths = glob.glob(rootdir_training + "/0/*.png")
+train_labels =[]
+training = []
+count = 0
+for img in img_paths[:]:
+    if count > 100:
+        break
+    label = img.split("/")[-2]
+    print label
+    data = cv2.imread(img)
+    #vec = hog2.compute(data)
+    gradients, angles = apply_kernel(data)
+    vec = bin_gradients(gradients, angles)
+    vec = normalize_hog(vec)
+    vec = concatenate(vec)
+    train_labels.append(label)
+    training.append(vec)
+    count += 1
+img_paths = glob.glob(rootdir_testing + "/0/*.png")
+test_labels =[]
+testing = []
+count = 0
+for img in img_paths[:]:
+    if count > 100:
+        break
+    count += 1
+    label = img.split("/")[-2]
+    print label
+    data = cv2.imread(img)
+    #vec = hog2.compute(data)
+    gradients, angles = apply_kernel(data)
+    vec = bin_gradients(gradients, angles)
+    vec = normalize_hog(vec)
+    vec = concatenate(vec)
+    test_labels.append(label)
+    testing.append(vec)
+'''
+train_labels =[]
+training = []
+test_labels =[]
+testing = []
+for x in range(0, 10):
+    count = 0
+    img_paths = glob.glob(rootdir_training + "/" + str(x) + "/*.png")
+    for img in img_paths[:]:
+        if count > 200:
+            break
+        label = img.split("/")[-2]
+        print label
+        data = cv2.imread(img)
+        #vec = hog2.compute(data)
+        data = cv2.resize(data, (0, 0), fx=2, fy=4)
+        gradients, angles = apply_kernel(data)
+        vec = bin_gradients(gradients, angles)
+        vec = normalize_hog(vec)
+        vec = concatenate(vec)
+        train_labels.append(label)
+        training.append(vec)
+        count += 1
+    img_paths = glob.glob(rootdir_testing + "/" + str(x) + "/*.png")
+    count = 0
+    for img in img_paths[:]:
+        if count > 200:
+            break
+        count += 1
+        label = img.split("/")[-2]
+        print label
+        data = cv2.imread(img)
+        data = cv2.resize(data, (0, 0), fx=2, fy=4)
+        #vec = hog2.compute(data)
+        gradients, angles = apply_kernel(data)
+        vec = bin_gradients(gradients, angles)
+        vec = normalize_hog(vec)
+        vec = concatenate(vec)
+        test_labels.append(label)
+        testing.append(vec)
+#nsamples, nx, ny = np.asarray(training).shape
+#training = np.asarray(training).reshape((nsamples,nx*ny))
+#nsamples, nx, ny = np.asarray(testing).shape
+#testing = np.asarray(testing).reshape((nsamples,nx*ny))
+
+clf = svm.SVC()
+clf.fit(training, train_labels)
+predict = clf.predict(testing)
+print metrics.accuracy_score(predict, test_labels)
---
+++ OpticalFlow.py
@@ -0,0 +1,215 @@
+import cv2
+from scipy import misc
+import matplotlib.pyplot as plt
+import os
+import numpy as np
+import math
+from sklearn import datasets, svm, metrics, tree, ensemble, linear_model
+import glob
+
+original_frame1 = cv2.imread("tennis492.jpg")
+original_frame2 = cv2.imread("tennis493.jpg")
+
+'''
+Make sure we don't access outside of array
+'''
+def boundIndex(index, maxIndex):
+    if index < 0:
+        return 0
+    if index >= maxIndex:
+        return maxIndex - 1
+    return index
+
+
+def calcMatrixValues(curr_row, curr_col, prev_frame, curr_frame):
+    lucas_kanade_matrix = np.zeros((2,2),dtype=np.float64)
+    lucas_kanade_values = np.zeros((2,1),dtype=np.float64)
+    lucas_kanade_optical_flow = np.zeros((2,1),dtype=np.float64)
+    rows = len(prev_frame)
+    columns = len(prev_frame[0])
+    for i in range(curr_row-1, curr_row+2):
+        for j in range(curr_col-1, curr_col+2):
+            # dx of curr_frame
+            rIndex = boundIndex(j + 1, columns)
+            lIndex = boundIndex(j - 1, columns)
+            tIndex = boundIndex(i - 1, rows)
+            bIndex = boundIndex(i + 1, rows)
+            dx = np.float64(curr_frame[i][rIndex] - curr_frame[i][lIndex])
+            dy = np.float64(curr_frame[bIndex][j] - curr_frame[tIndex][j])
+            dt = np.float64(curr_frame[i][j] - prev_frame[i][j])
+
+            lucas_kanade_matrix[0][0] = dx * dx + lucas_kanade_matrix[0][0]
+            lucas_kanade_matrix[0][1] = dx * dy + lucas_kanade_matrix[0][1]
+            lucas_kanade_matrix[1][0] = dx * dy + lucas_kanade_matrix[1][0]
+            lucas_kanade_matrix[1][1] = dy * dy + lucas_kanade_matrix[1][1]
+
+            lucas_kanade_values[0][0] = dx * dt + lucas_kanade_values[0][0]
+            lucas_kanade_values[1][0] = dy * dt + lucas_kanade_values[1][0]
+    # now invert lucas_kanade_matrix
+    try:
+        #lucas_kanade_matrix = np.linalg.inv(lucas_kanade_matrix)
+        lucas_kanade_values[0][0] = -1 * lucas_kanade_values[0][0]
+        lucas_kanade_values[1][0] = -1 * lucas_kanade_values[1][0]
+        a = lucas_kanade_matrix[0][0]
+        b = lucas_kanade_matrix[0][1]
+        c = lucas_kanade_matrix[1][0]
+        d = lucas_kanade_matrix[1][1]
+        det = a*d - b * c
+        if (det == 0):
+            lucas_kanade_optical_flow[0][0] = 0
+            lucas_kanade_optical_flow[1][0] = 0
+        else:
+            lucas_kanade_matrix[0][0] = d / det
+            lucas_kanade_matrix[1][1] = a / det
+            lucas_kanade_matrix[0][1] = -1 * b / det
+            lucas_kanade_matrix[1][0] = -1 * c / det
+            lucas_kanade_optical_flow = np.dot(lucas_kanade_matrix, lucas_kanade_values)
+        '''
+        lucas_kanade_optical_flow[0][0] = lucas_kanade_matrix[0][0] * lucas_kanade_values[0][0] + lucas_kanade_matrix[0][1] * lucas_kanade_values[1][0]
+        lucas_kanade_optical_flow[1][0] = lucas_kanade_matrix[1][0] * lucas_kanade_values[0][0] + \
+                                          lucas_kanade_matrix[1][1] * lucas_kanade_values[1][0]
+                                          '''
+    except np.linalg.linalg.LinAlgError:
+        # I guess just set the flow equal to 0 if the matrix isn't invertible
+        lucas_kanade_optical_flow[0][0] = 0
+        lucas_kanade_optical_flow[1][0] = 0
+    return lucas_kanade_optical_flow
+
+
+def calcOpticalFlow(prev_frame, curr_frame):
+    # u is x component, v is y component
+    # lucas kanade takes a 5x5 patch around each point
+    # I'll leave aside the edges for now
+    rows = len(prev_frame)
+    cols = len(prev_frame[0])
+    # create matrix for storing flow u and v
+    flows = np.zeros((rows,cols,2),dtype=np.float64)
+    for i in range(2, rows - 2):
+        for j in range(2, cols - 2):
+            # get the 25 points and calculate u and v for them
+            curr_flow = calcMatrixValues(i,j,prev_frame, curr_frame)
+            '''
+            grads = curr_flow.astype(np.uint64)
+            mag = np.sqrt(curr_flow[0][0] ** 2 + curr_flow[1][0] ** 2)
+            angle = np.arctan2(grads[1][0], grads[0][0])
+            angle = angle * 180 / np.pi
+            flows[i][j][0] = mag
+            flows[i][j][1] = angle
+            '''
+            #angle = np.uint8(abs(angle))
+            # opencv returns calculated new positions
+            flows[i][j][0] = curr_flow[0][0]
+            flows[i][j][1] = curr_flow[1][0]
+    # now that we're done, return flows
+    return flows
+
+
+# convert to gray-scale
+frame1 = cv2.cvtColor(original_frame1,cv2.COLOR_BGR2GRAY)
+frame2 = cv2.cvtColor(original_frame2,cv2.COLOR_BGR2GRAY)
+
+
+
+'''
+plt.subplot(1,2,1),plt.imshow(frame1, cmap="gray"),plt.title("Frame 1"),plt.xticks([]), plt.yticks([])
+plt.subplot(1,2,2),plt.imshow(frame2, cmap="gray"),plt.title("Frame 2"),plt.xticks([]), plt.yticks([])
+plt.show()
+'''
+
+# now need to compute optical flow -> for now just focus on calculating u and v, and don't worry about gradients in x and y direction
+frame3 = np.zeros((len(frame1), len(frame1[0])), dtype=np.float64)
+frame4 = np.zeros((len(frame1), len(frame1[0])), dtype=np.float64)
+
+for i in range(0, len(frame1)):
+    for j in range(0, len(frame1[0])):
+        frame3[i][j] = np.float64(frame1[i][j] / 255.)
+        frame4[i][j] = np.float64(frame2[i][j] / 255.)
+
+flow = calcOpticalFlow(frame3, frame4)
+
+'''
+gradFlow = np.zeros((len(flow), len(flow[0])), dtype=np.uint64)
+# get magnitude and direction
+for i in range(1, len(flow) - 1):
+    for j in range(1, len(flow[0]) - 1):
+        mag = flow[i][j][0] ** 2 + flow[i][j][1] ** 2
+        gradFlow[i][j] = np.sqrt(mag)
+        angle = np.arctan2(flow[i][j][0], flow[i][j][1])
+        #angle = angle * 180 / np.pi
+        #angle = np.uint8(abs(angle))
+        #gradFlow[i][j][1] = angle
+'''
+
+# params for ShiTomasi corner detection
+'''
+feature_params = dict( maxCorners = 100,
+                       qualityLevel = 0.3,
+                       minDistance = 7,
+                       blockSize = 7 )
+
+p0 = cv2.goodFeaturesToTrack(frame1, mask = None, **feature_params)
+'''
+#flow = cv2.calcOpticalFlowPyrLK(frame1, frame2, p0)
+
+prvs = frame1
+next = frame2
+initial_flow = None
+pyr_scale = 0.5
+levels = 3
+winsize = 15
+iterations = 3
+poly_n = 5
+poly_sigma = 1.1
+flags = 0
+
+their_flow = cv2.calcOpticalFlowFarneback(prvs, next, 0, 0, 3, 15, 3, 5, 1, 0)
+
+
+'''
+mag, ang = cv2.cartToPolar(flow[...,0], flow[...,1])
+
+hsv = np.zeros_like(original_frame1)
+hsv[...,1] = 255
+hsv[...,0] = ang * 180 / np.pi / 2
+hsv[...,2] = cv2.normalize(mag,None,0,255,cv2.NORM_MINMAX)
+rgb = cv2.cvtColor(hsv,cv2.COLOR_HSV2BGR)
+
+plt.subplot(1,1,1),plt.imshow(rgb),plt.title("RGB")
+plt.show()
+'''
+st2 = 1 # variable to reduce density of the vector field
+
+Uf = np.flipud(flow[...,0]) # horizontal optical flow
+Vf = np.flipud(-flow[...,1]) # vertical optical flow
+
+Uf2 = np.flipud(their_flow[...,0]) # horizontal optical flow
+Vf2 = np.flipud(-their_flow[...,1]) # vertical optical flow
+Q2 = plt.quiver(Uf2, Vf2)
+
+Q = plt.quiver(Uf,Vf)
+
+plt.subplot(1,2,1),plt.quiver(Uf, Vf),plt.title("My optical flow"),plt.xticks([]), plt.yticks([])
+plt.subplot(1,2,2),plt.quiver(Uf2, Vf2),plt.title("Their optical flow"),plt.xticks([]), plt.yticks([])
+
+plt.show()
+
+
+Uf_dense = np.flipud(flow[...,0]) # horizontal optical flow
+Vf_dense = np.flipud(-flow[...,1]) # vertical optical flow
+m,n = Uf_dense.shape
+y,x = np.mgrid[0.:m, 0.:n]
+
+pct = 97
+mag, ang = cv2.cartToPolar(flow[...,0], flow[...,1])
+mag_threshold = np.percentile(mag.ravel(), pct)
+
+x[mag<mag_threshold] = np.nan
+y[mag<mag_threshold] = np.nan
+
+plt.subplot(1,2,1)
+Q = plt.quiver(x,y,Uf_dense,Vf_dense,color='r')
+plt.imshow(frame1, cmap="gray"),plt.title("Frame 1")
+plt.subplot(1,2,2)
+Q = plt.quiver(x,y,Uf_dense,Vf_dense,color='r')
+plt.imshow(frame2, cmap="gray"),plt.title("Frame 2")
+plt.show()
